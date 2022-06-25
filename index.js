639e2f06535ca4af834bea74731d2107fb8bdeea
Udosoh <105041343+Udosoh@users.noreply.github.com>
2022-06-25 23:29:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+let number1 =prompt("enter  number")
+let symbol =prompt("enter operand")
+let number2 =prompt("enter number")
+
+
+function calaculator(number1, symbol, number2) {
+ 
+
+
+if( typeof number1 === "number"  && (symbol == "+" || symbol == "-" || symbol == "*" 
+|| symbol == "/"  || symbol == "*"   ) &&  typeof number2 ==="number"){
+
+} else return "invalid" ;
+
+
+}
+
+
