17605e5bf51555444ad9d6bdd701d34caa10167d
Udosoh <105041343+Udosoh@users.noreply.github.com>
2022-07-01 00:11:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 
 
-let number1 =prompt("enter  number")
-let symbol =prompt("enter operand")
-let number2 =prompt("enter number")
+let number1 =parseFloat(prompt("enter  number"));
+let operand =prompt("enter operand")
+let number2 =parseFloat(prompt("enter  number"));
 
+let result;
 
-function calaculator(number1, symbol, number2) {
- 
+if(operand == "+"){
+ result = number1 + number"
+} else if (operand == "-"){
+result = number1 -number2
+} else if  (operand == "/"){
+result = number1 / number2 
+ } else if  (operand == "*"){
+result = number1 * number2 }
+else { promt("enter correct operand either -,+,/,* ")}
 
-
-if( typeof number1 === "number"  && (symbol == "+" || symbol == "-" || symbol == "*" 
-|| symbol == "/"  || symbol == "*"   ) &&  typeof number2 ==="number"){
-
-} else return "invalid" ;
-
-
-}
-
+windows.alert(`its equal to ${result}`)
 
